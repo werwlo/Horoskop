4d64891db03a5d58db0263b3e44b6287ee2aa355
gorzi-coder <51759618+gorzi-coder@users.noreply.github.com>
2019-11-20 19:00:16 +0100

    Update views.py

--- uploads/core/views.py
+++ uploads/core/views.py
@@ -10,7 +10,6 @@ def about(request):
     print('O astrologii')
     return render(request, 'about.html')
 
-
 def contact(request):
     print('Kontakt')
     return render(request, 'contact.html')
@@ -18,6 +17,25 @@ def contact(request):
 def koziorozec(request):
     print('Koziorożec')
     return render(request, 'koziorozec.html')
+
+def wodnik(request):
+    print('Wodnik')
+    return render(request, 'wodnik.html')
+
+def ryby(request):
+    print('Ryby')
+    return render(request, 'ryby.html')
+
+def baran(request):
+    print('Baran')
+    return render(request, 'baran.html')
+
+def byk(request):
+    print('Byk')
+    return render(request, 'byk.html')
+
+
+
 
 def home(request):
     print('Data analysis')
